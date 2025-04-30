bd4488cbda775f0539d3fce5ac762af593ed47cb
nkosi <nkoc.mkhize18@gmail.com>
2025-04-30 16:51:30 +0200

    Add Exception CLass to handle exceptions

---
+++ patient-service/src/main/java/com/codewithnkosi/patientservice/exception/PatientServiceException.java
@@ -0,0 +1,24 @@
+package com.codewithnkosi.patientservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@ControllerAdvice
+public class PatientServiceException {
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getFieldErrors().forEach(error -> {
+            String field = error.getField();
+            String message = error.getDefaultMessage();
+            errors.put(field, message);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+}
