89f249f3993fd34bb8ae0bd8a5c7d209b6b23750
nkosi <nkoc.mkhize18@gmail.com>
2025-06-07 01:59:19 +0200

    GRPC and Protobuf

--- patient-service/src/main/java/com/codewithnkosi/patientservice/PatientServiceApplication.java
+++ patient-service/src/main/java/com/codewithnkosi/patientservice/PatientServiceApplication.java
@@ -3,6 +3,7 @@ package com.codewithnkosi.patientservice;
 import com.codewithnkosi.patientservice.model.Patient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.*;
 import java.util.stream.Collectors;
--- patient-service/src/main/java/com/codewithnkosi/patientservice/service/PatientService.java
+++ patient-service/src/main/java/com/codewithnkosi/patientservice/service/PatientService.java
@@ -1,24 +1,30 @@
 package com.codewithnkosi.patientservice.service;
 
+import billing.BillingResponse;
 import com.codewithnkosi.patientservice.dto.PatientRequestDTO;
 import com.codewithnkosi.patientservice.dto.PatientResponseDTO;
 import com.codewithnkosi.patientservice.exception.EmailAlreadyExistsException;
 import com.codewithnkosi.patientservice.exception.PatientNotFoundException;
+import com.codewithnkosi.patientservice.grpc.BillingServiceGrpcClient;
 import com.codewithnkosi.patientservice.mapper.PatientMapper;
 import com.codewithnkosi.patientservice.model.Patient;
 import com.codewithnkosi.patientservice.repository.PatientRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class PatientService {
     private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
-    public PatientService(PatientRepository patientRepository) {
+    public PatientService(PatientRepository patientRepository, BillingServiceGrpcClient billingServiceGrpcClient) {
         this.patientRepository = patientRepository;
+        this.billingServiceGrpcClient = billingServiceGrpcClient;
     }
 
     public List<PatientResponseDTO> getAllPatients() {
@@ -31,6 +37,11 @@ public class PatientService {
             throw new EmailAlreadyExistsException("Email already exists " + patientRequestDTO.getEmail());
         }
         Patient patient = patientRepository.save(PatientMapper.toPatient(patientRequestDTO));
+
+        log.info("Creating Billing account for patient {}", patient.getId());
+        BillingResponse billingAccount = billingServiceGrpcClient.createBillingAccount(patient.getId().toString(), patient.getName(), patient.getEmail());
+        log.info("Billing account created successfully for patient {}, account {}", patient.getId(), billingAccount);
+
         return PatientMapper.toPatientResponseDTO(patient);
     }
 
@@ -53,4 +64,5 @@ public class PatientService {
     public void deletePatient(UUID id) {
         patientRepository.deleteById(id);
     }
+
 }
